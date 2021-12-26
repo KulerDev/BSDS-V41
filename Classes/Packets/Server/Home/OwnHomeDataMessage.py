9b3f23dc66d0371d7e6c4d046adab78231aaf2f9
Kuler <69260523+KulerDev@users.noreply.github.com>
2021-12-26 14:53:11 +0300

    Update Theme

--- Classes/Packets/Server/Home/OwnHomeDataMessage.py
+++ Classes/Packets/Server/Home/OwnHomeDataMessage.py
@@ -408,7 +408,7 @@ class OwnHomeDataMessage(PiranhaMessage):
 
         self.writeLong(10008, 501)
         self.writeLong(65, 2)
-        self.writeLong(1, 41000036)  # ThemeID
+        self.writeLong(1, 41000038)  # ThemeID
         self.writeLong(60, 36270)
         self.writeLong(66, 1)
         self.writeLong(61, 36270)  # SupportDisabled State | if 36218 < state its true
